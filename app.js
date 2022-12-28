9df4067c5cfe97a18ceb5d7c77d6bf596d915baa
uniq47 <unique.sapkota01@student.csulb.edu>
2022-12-28 08:55:09 -0800

    type annotation and type inferance

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("running ");
+//type annotation
+var movieTitle = "Name"; // here we are doing it ourself its called type annotation
+//Type inferance
+movieTitle = "New Name";
+var anything;
+//anything = "Nepal";
+anything = true;
--- app.ts
+++ app.ts
@@ -1,1 +1,13 @@
-console.log("running ")
+//type annotation
+let movieTitle: string = "Name" // here we are doing it ourself its called type annotation
+//Type inferance
+movieTitle= "New Name"
+
+
+let anything: boolean;
+
+//anything = "Nepal";
+anything = true;
+
+
+
