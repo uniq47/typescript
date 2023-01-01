d962a85f3306deda48c493fcc954974bca5756bc
uniq47 <unique.sapkota01@student.csulb.edu>
2022-12-31 17:11:56 -0800

    tsc config.json created

--- app.js
+++
@@ -1,7 +0,0 @@
-//type annotation
-var movieTitle = "Name"; // here we are doing it ourself its called type annotation
-//Type inferance
-movieTitle = "New Name";
-var anything;
-//anything = "Nepal";
-anything = true;
