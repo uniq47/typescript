6fd1a2871be85b4a69e78c65bde893a4bebae73e
uniq47 <unique.sapkota01@student.csulb.edu>
2023-01-01 12:42:45 -0800

    any and unknown keywords

--- dist/app.js
+++ dist/app.js
@@ -1,64 +1,28 @@
 "use strict";
-// primitive  types
-const isAvailable = true;
-//let isAvailableLet = 20;
-let age = 30;
-let hello = "Hello";
-let notDefined;
-let notPresent = null;
-notPresent = 30;
-console.log(Symbol("unique") === Symbol("unique"));
-//let bigNUmber = BigInt(10);
-//console.log(bigNUmber);
-//let bigNUmber = 20n; 
-//bulding constructor finction in js
-let set = new Set(["hello"]);
-console.log(set);
-//let arr=[1,2,3,4];
-//let arr = new Array<number>(1,2,3,4);
-let arr = new Array(1, 2, 3, 4);
-//there two are the same
-let regexp = new RegExp('hello');
-//const passCheck = /hello/
-const passCheck = /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$%&? "])[a-zA-Z0-9!#$%&?]{8,20}$/;
-const myPassword = "Kathmandu1";
-if (myPassword.match(passCheck)) {
-    console.log("Password is strong");
+let exampleAny;
+exampleAny = 10;
+console.log(typeof exampleAny);
+exampleAny = 'Hello';
+exampleAny = [1, 2, 3];
+console.log(exampleAny);
+let exampleUnknown;
+exampleUnknown = 10;
+console.log(typeof exampleUnknown);
+exampleUnknown = 'Hello';
+exampleUnknown = [1, 2, 3];
+let numbers;
+numbers = 40;
+numbers = 50.1;
+numbers = 15.44;
+// console.log(numbers.toFixed(1));
+// numbers.i.wish.to.usethis();
+// any->unknown
+if (typeof numbers === "number") {
+    console.log("numbers is a number");
+    console.log(numbers.toFixed(1));
 }
-else {
-    console.log("Password is weak");
+if (typeof numbers === "string") {
+    console.log("numbers is a string");
+    console.log(numbers.toUpperCase());
 }
-const map = new Map();
-//git add -A && git commit -m "message" && git push all at once 
-//git config --global alias.cmp '!f() { git add -A && git commit -m "$@" && git push; }; f'
-//git cmp " message";
-class Queue {
-    constructor() {
-        this.data = [];
-    }
-    push(item) {
-        this.data.push(item);
-    }
-    pop() {
-        return this.data.shift();
-    }
-}
-const firstObj = new Queue();
-firstObj.push(1);
-firstObj.push("2");
-console.log(firstObj);
-;
-const obj = {
-    firstName: "uniq",
-    age: "23"
-};
-obj.lastName = "Sap";
-const hisObj = {
-    firstName: "uniq",
-    age: "23"
-};
-//Tuples 
-let numbers = [1, 2];
-numbers = [5555, 5];
-//numbers.push(30);
-console.log(numbers);
+typeof numbers === "number" && console.log(numbers.toFixed(1));
--- src/app.ts
+++ src/app.ts
@@ -1,96 +1,36 @@
+let exampleAny;
+
+exampleAny = 10;
+console.log(typeof exampleAny);
+exampleAny = 'Hello';
+exampleAny = [1,2,3];
+
+console.log(exampleAny);
+
+let exampleUnknown: unknown;
+exampleUnknown = 10;
+console.log(typeof exampleUnknown);
+exampleUnknown = 'Hello';
+exampleUnknown = [1,2,3];
 
 
+let numbers: unknown;
+numbers =40;
+numbers = 50.1;
+numbers =15.44;
 
-// primitive  types
-const isAvailable: boolean = true;
-//let isAvailableLet = 20;
+// console.log(numbers.toFixed(1));
+// numbers.i.wish.to.usethis();
 
-let age =30;
-let hello = "Hello";
+// any->unknown
 
-let notDefined;
-let notPresent: number | null = null;
-notPresent = 30;
-
-console.log(Symbol("unique") === Symbol("unique"));
-
-//let bigNUmber = BigInt(10);
-//console.log(bigNUmber);
-
-//let bigNUmber = 20n; 
-
-//bulding constructor finction in js
-let set = new Set<string>(["hello"]);
-console.log(set);
-
-
-//let arr=[1,2,3,4];
-//let arr = new Array<number>(1,2,3,4);
-let arr: number[] = new Array(1,2,3,4);
-//there two are the same
-let regexp = new RegExp('hello');
-
-//const passCheck = /hello/
-
-const passCheck = /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$%&? "])[a-zA-Z0-9!#$%&?]{8,20}$/;
-
-const myPassword = "Kathmandu1";
-
-if(myPassword.match(passCheck)){
-    console.log("Password is strong");
+if(typeof numbers ===  "number"){
+    console.log("numbers is a number");
+    console.log(numbers.toFixed(1));
 }
-else{
-    console.log("Password is weak");
+if(typeof numbers === "string"){
+    console.log("numbers is a string");
+    console.log(numbers.toUpperCase());
 }
 
- const map = new Map<string, number>()
-//git add -A && git commit -m "message" && git push all at once 
-//git config --global alias.cmp '!f() { git add -A && git commit -m "$@" && git push; }; f'
-//git cmp " message";
-
-class Queue<T>{
-    data: Array<T> = [];
-
-    push(item: T){
-        this.data.push(item);
-    }
-
-    pop(): T | undefined{
-        return this.data.shift();
-    }
-}
-
-const firstObj = new Queue();
-
-firstObj.push(1);
-firstObj.push("2");
-
-console.log(firstObj);
-
-
-//type alias // lastname is optional property so we use ? 
-//type Person = {firstName: string;age: string ; lastName?: string | undefined};
-//we can write interface so name fof the interface and this is sdame as type
-interface Person{
-    firstName: string;
-    age: string ; 
-    lastName?: string | undefined};
-
-
-const obj: Person={
-    firstName: "uniq",
-    age: "23"
-};
-
-obj.lastName="Sap"
-const hisObj: Person={
-    firstName: "uniq",
-    age: "23"
-};
-
-//Tuples 
-let numbers: readonly[number,number] = [1,2];
-
-numbers=[5555,5]
-//numbers.push(30);
-console.log(numbers);
+typeof numbers === "number" && console.log(numbers.toFixed(1));
