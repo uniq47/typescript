cfde94be84e1f7ffcb2e5d944058b39bb179620c
uniq47 <unique.sapkota01@student.csulb.edu>
2022-12-31 19:15:05 -0800

    primitive types

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log("yo");
+// primitive  types
+const isAvailable = true;
+//let isAvailableLet = 20;
+let age = 30;
+let hello = "Hello";
+let notDefined;
+let notPresent = null;
+notPresent = 30;
+console.log(Symbol("unique") === Symbol("unique"));
+//let bigNUmber = BigInt(10);
+//console.log(bigNUmber);
+let bigNUmber = 20n;
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,22 @@
-console.log("yo");
+
+
+
+// primitive  types
+const isAvailable: boolean = true;
+//let isAvailableLet = 20;
+
+let age =30;
+let hello = "Hello";
+
+let notDefined;
+let notPresent: number | null = null;
+notPresent = 30;
+
+console.log(Symbol("unique") === Symbol("unique"));
+
+//let bigNUmber = BigInt(10);
+//console.log(bigNUmber);
+
+let bigNUmber = 20n; 
+
+
