d653f5dacc5d689f79fd5f759078a34c3711e4e8
uniq47 <unique.sapkota01@student.csulb.edu>
2022-12-31 19:46:41 -0800

    class in TS

--- dist/app.js
+++ dist/app.js
@@ -10,4 +10,25 @@ notPresent = 30;
 console.log(Symbol("unique") === Symbol("unique"));
 //let bigNUmber = BigInt(10);
 //console.log(bigNUmber);
-let bigNUmber = 20n;
+//let bigNUmber = 20n; 
+//bulding constructor finction in js
+let set = new Set(["hello"]);
+console.log(set);
+//let arr=[1,2,3,4];
+//let arr = new Array<number>(1,2,3,4);
+let arr = new Array(1, 2, 3, 4);
+//there two are the same
+let regexp = new RegExp('hello');
+//const passCheck = /hello/
+const passCheck = /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$%&? "])[a-zA-Z0-9!#$%&?]{8,20}$/;
+const myPassword = "Kathmandu1";
+if (myPassword.match(passCheck)) {
+    console.log("Password is strong");
+}
+else {
+    console.log("Password is weak");
+}
+const map = new Map();
+//git add -A && git commit -m "message" && git push all at once 
+//git config --global alias.cmp '!f() { git add -A && git commit -m "$@" && git push; }; f'
+//git cmp " message";
--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,33 @@ console.log(Symbol("unique") === Symbol("unique"));
 //let bigNUmber = BigInt(10);
 //console.log(bigNUmber);
 
-let bigNUmber = 20n; 
+//let bigNUmber = 20n; 
+
+//bulding constructor finction in js
+let set = new Set<string>(["hello"]);
+console.log(set);
 
 
+//let arr=[1,2,3,4];
+//let arr = new Array<number>(1,2,3,4);
+let arr: number[] = new Array(1,2,3,4);
+//there two are the same
+let regexp = new RegExp('hello');
+
+//const passCheck = /hello/
+
+const passCheck = /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$%&? "])[a-zA-Z0-9!#$%&?]{8,20}$/;
+
+const myPassword = "Kathmandu1";
+
+if(myPassword.match(passCheck)){
+    console.log("Password is strong");
+}
+else{
+    console.log("Password is weak");
+}
+
+ const map = new Map<string, number>()
+//git add -A && git commit -m "message" && git push all at once 
+//git config --global alias.cmp '!f() { git add -A && git commit -m "$@" && git push; }; f'
+//git cmp " message";
